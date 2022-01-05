926ea9b71e36dcf1f23aef1b7a0e3ca3a4a1b58e
Cedric Vidonne <cedric@vidonne.me>
2022-01-05 14:09:55 +0100

    feat: UNHCR variables for bslib

---
+++ R/unhcr_bslib.R
@@ -0,0 +1,8 @@
+#' UNHCR bslib variables
+#'
+#' Bootstrap default to be used in bslib for UNHCR
+#'
+#' @format A list
+#' @examples
+#' unhcr_bslib
+"unhcr_bslib"
---
+++ data-raw/gen_unhcr_bslib.R
@@ -0,0 +1,31 @@
+library(usethis)
+
+unhcr_bslib <- list(
+  version = 5,
+  `variable-prefix` = "unhcr-",
+  `enable-rounded` = FALSE,
+  `gray-100` = "#f2f2f2",
+  `gray-200` = "#E6E6E6",
+  `gray-300` = "#CCCCCC",
+  `gray-400` = "#b3b3b3",
+  `gray-500` = "#999999",
+  `gray-600` = "#666666",
+  `gray-700` = "#4d4d4d",
+  `gray-800` = "#333333",
+  `gray-900` = "#222222",
+  blue = "#0072BC",
+  yellow = "#FAEB00",
+  green = "#00B398",
+  red = "#EF4A60",
+  info = "#8EBEFF",
+  warning = "#E1CC0D",
+  base_font = bslib::font_google("Lato"),
+  code_font = bslib::font_google("Source Code Pro"),
+  `link-decoration` = "none",
+  `link-hover-color` = "#222222",
+  `link-hover-decoration` = "underline",
+  `blockquote-footer-color` = "#222222",
+  `mark-bg` = "#FFF9CB"
+)
+
+usethis::use_data(unhcr_bslib, overwrite = TRUE)
